81286c316f9d54c6ba1037816af7a3001dbb7619
Davy <davy@thechatbotfactory.com>
2018-07-17 13:49:45 +0200

    login WIP

--- server/model/Login.js
+++ server/model/Login.js
@@ -12,30 +12,6 @@ const LoginSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  passwordConf: {
-    type: String,
-    required: true,
-  }
 }, {collection: 'users'})
-
-LoginSchema.statics.authenticate = (email, password, callback) {
-  Login.findOne({email})
-    .exec((err, user) {
-      if (err) {
-        return callback(err)
-      } else if (!user){
-        const err = new Error('User not found')
-        err.status = 401
-        return callback(err)
-      }
-      bcrypt.compare(password, user.password, (err, result) {
-        if (result === true) {
-          return callback(null, user)
-        } else {
-          return callback()
-        }
-      })
-    })
-}
 
 module.exports = mongoose.model('Login', LoginSchema)
--- server/routes/apiLogin.js
+++ server/routes/apiLogin.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const app = express()
+const bcrypt = require('bcrypt')
+const Login = require('../model/Login')
 const { API_KEY } = require('../constants')
 
 router.post('/', (req, res) => {
@@ -10,24 +12,34 @@ router.post('/', (req, res) => {
     password
    } = req.body
 
-  if(API_KEY === apiKey) {
-    Login.authenticate(email, password, (err, user) => {
-      if(error || !user) {
-        const err = new Error('Wrong email or password')
-        err.status = 401
-        return next(err)
-      } else {
-        req.session.userId = user._id
-        console.log('logged in')
-      }
-    })
-
-  } else {
-    const err = new Error('All fields required')
-    err.status = 400
-    return next(err)
+  if(API_KEY !== apiKey) {
+   res.status(400)
+   res.json({code: "VIBES_BAD_API_KEY", message: "Bad API key"})
   }
 
+  if(API_KEY === apiKey) {
+    Login.findOne({email}, (err, user) => {
+      if(err) {
+        res.status(400)
+        res.json({code: "VIBES_NOT_AVAILABLE", message: "Une opération de maintenance est en cours. Veuillez nous excuser pour la gêne occasionnée, on revient vite!"})
+      } else {
+        if(user) {
+          bcrypt.compare(password, user.password, (err, result) => {
+            if(result === true) {
+              res.status(200)
+              res.json({userId: user._id, userToken: '', lastActiveDate: '', refreshToken: ''})
+            } else {
+              res.status(400)
+              res.json({code: "VIBES_BAD_LOGPWD", message: "Identifiants non reconnus"})
+            }
+          })
+        } else {
+          res.status(400)
+          res.json({code: "VIBES_BAD_LOGPWD", message: "Identifiants non reconnus"})
+        }
+      }
+    })
+  }
 })
 
 module.exports = router
