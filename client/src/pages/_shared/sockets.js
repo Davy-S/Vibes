26ef751f56cb8ebc2715751c91c8b2fa1efbaf5d
Davy-S <davy.simonin@gmail.com>
2018-08-01 14:00:55 +0200

    truc

--- client/src/pages/_shared/sockets.js
+++ client/src/pages/_shared/sockets.js
@@ -2,6 +2,7 @@ import openSocket from 'socket.io-client'
 const options = {
   rejectUnauthorized: false // allow self-signed certs
 }
-const socket = openSocket('http://localhost:5000', options)
+//https://vibes-app.herokuapp.com/
+const socket = openSocket('https://vibes-app.herokuapp.com/', options)
 
 export default socket
