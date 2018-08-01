c3c9ed9f1e0bc0484d7f6f067dba332a264cd940
Davy-S <davy.simonin@gmail.com>
2018-08-01 14:18:48 +0200

    sockets

--- client/src/pages/_shared/sockets.js
+++ client/src/pages/_shared/sockets.js
@@ -3,6 +3,7 @@ const options = {
   rejectUnauthorized: false // allow self-signed certs
 }
 //https://vibes-app.herokuapp.com/
+//http://localhost:5000
 const socket = openSocket('https://vibes-app.herokuapp.com/', options)
 
 export default socket
